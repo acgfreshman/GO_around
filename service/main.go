d758f35288adb730e2dc0524ccb18f27b70daffb
Muchen Liu <liumuchen1996@gmail.com>
2018-03-28 19:47:05 -0700

    modify elasticsearch external ip

--- service/main.go
+++ service/main.go
@@ -30,7 +30,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.229.105.11:9200"
+	ES_URL = "http://35.185.58.172:9200"
 )
 
 
