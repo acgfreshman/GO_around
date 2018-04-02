c8024789a46f80897a024a8cb6d3970d3695b278
Muchen Liu <liumuchen1996@gmail.com>
2018-04-02 15:10:52 -0700

    add google cloud and bigtable dependency

---
+++ dataflow/src/main/java/com/around/PostDumpFlow.java
@@ -0,0 +1,19 @@
+package com.around;
+
+/**
+ * Created by muchen liu on 2018/4/2.
+ */
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.options.PipelineOptions;
+import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
+
+public class PostDumpFlow {
+    public static void main(String[] args) {
+        // Start by defining the options for the pipeline.
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+
+        Pipeline p = Pipeline.create(options);
+
+        p.run();
+    }
+}
